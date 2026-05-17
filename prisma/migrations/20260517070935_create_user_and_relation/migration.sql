22efe3523c8ad4ce75b6062dd0aece4a590e4ee2
SyahdanIR <izzursyahdan@gmail.com>
2026-05-17 15:07:38 +0700

    Add: Filtering, Sorting, Pagination, Inclusion

---
+++ prisma/migrations/20260517070935_create_user_and_relation/migration.sql
@@ -0,0 +1,25 @@
+/*
+  Warnings:
+
+  - Added the required column `userId` to the `Product` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Product" ADD COLUMN     "userId" INTEGER NOT NULL;
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -9,26 +9,66 @@ export const createProduct = async (req: Request, res: Response) => {
         name: name,
         price: Number(price),
         stock: Number(stock),
-        description: description
+        description: description,
       },
     });
     return res.status(201).json({
-        message: "Product created successfully",
-        data: newProduct
+      message: "Product created successfully",
+      data: newProduct,
     });
   } catch (error) {
-    res.status(500).json({ 
-        message: "Failed to create product", 
-        error: error.message, 
+    res.status(500).json({
+      message: "Failed to create product",
+      error: error.message,
     });
   }
 };
 
 export const getAllProducts = async (req: Request, res: Response) => {
   try {
-    const products = await prisma.product.findMany();
+    const { search, minPrice, sortBy } = req.query;
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+
+    const skip = (page - 1) * limit;
+
+    const products = await prisma.product.findMany({
+      where: {
+        name: {
+          contains: search as string,
+          mode: "insensitive",
+        },
+        price: {
+          gte: minPrice ? Number(minPrice) : 0
+        },
+      },
+      take: limit,
+      skip: skip,
+      orderBy: {
+        CreatedAt: sortBy === "oldest" ? "asc" : "desc",
+      },
+      include: {
+        author: {
+          select: {
+            id: true,
+            name: true,
+            email: true,
+          },
+        },
+      },
+    });
+
+    const totalData = await prisma.product.count();
+
     return res.status(200).json({
       message: "All products fetched successfully",
+      meta: {
+        current_page: page,
+        limit: limit,
+        total_data: totalData,
+        total_pages: Math.ceil(totalData / limit),
+      },
       data: products,
     });
   } catch (error) {
